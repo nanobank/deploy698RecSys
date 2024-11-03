4e0034777ddfaf3e98bc31975f3ffafe13dffbbc
nanobank <56763392+nanobank@users.noreply.github.com>
2024-11-03 14:41:33 +0700

    Update app_rec_movie_65130701934.py

--- app_rec_movie_65130701934.py
+++ app_rec_movie_65130701934.py
@@ -7,7 +7,7 @@ import requests
 # Define a function to load the data from GitHub
 @st.cache_data  # Cache the data to avoid re-downloading
 def load_data():
-    with open('./recommendation_movie_svd.pkl', 'rb' ) as file:
+    with open('.\recommendation_movie_svd.pkl', 'rb' ) as file:
         svd_model,movie_ratings,movies = pickle.load(file)
     return svd_model, movie_ratings, movies
 
